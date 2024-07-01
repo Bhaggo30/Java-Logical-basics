aeb136fbb37f869ff693d181a6f14e15da7e40ca
bhagyashree <bhaggo30july@gmail.com>
2024-07-01 16:26:12 +0530

    add the sum of the digit

---
+++ SumOfDigits.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class SumOfDigits {
+
+    public static void main(String[] args) {
+        int digit, sum = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int number = sc.nextInt();
+        while(number > 0)
+        {
+            digit = number % 10;
+            sum = sum + digit;
+            number = number / 10;
+        }
+        System.out.println("Sum of Digits: "+sum);
+    }
+}
+
+
