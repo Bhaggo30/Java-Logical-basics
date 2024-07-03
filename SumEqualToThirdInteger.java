6feb77b290f9f7e672b0d1aadea2bf625d8bce4f
bhagyashree <bhaggo30july@gmail.com>
2024-07-03 17:08:59 +0530

    check the sum of third digit

--- SumEqualToThirdInteger.java
+++ SumEqualToThirdInteger.java
@@ -4,6 +4,12 @@ public class SumEqualToThirdInteger {
     public static boolean sumOfTwo(int x, int y,int z){
         return  ((x+y) == z || (y+z)==x || (z+x)==y);
     }
+    public static boolean test(int x, int y, int z,boolean xyz){
+        if (xyz)
+            return (y>x || z>y );
+        return true;
+
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter first integer value ");
@@ -12,6 +18,9 @@ public class SumEqualToThirdInteger {
         int y = sc.nextInt();
         System.out.println("enter first integer value ");
         int z = sc.nextInt();
-        System.out.println("sum of given integer is "+sumOfTwo(x,y,z));
+        System.out.println("enter boolean result (true || false)");
+        boolean xyz = sc.nextBoolean();
+        System.out.println("sum of given integer is " +sumOfTwo(x,y,z));
+        System.out.println("boolean result"+test(x,y,z,xyz));
     }
 }
