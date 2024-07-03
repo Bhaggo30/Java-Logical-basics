c6e7505aa50d397df61d619bc13a528107110ac2
bhagyashree <bhaggo30july@gmail.com>
2024-07-03 12:40:15 +0530

    print the particular number of table

--- TableOfNumber.java
+++ TableOfNumber.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class TableOfNumber {
+import java.util.Scanner;
+
+public class TableOfNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter a number to a table to be printed");
+        int n = scanner.nextInt();
+        for (int i =0; i<10; i++){
+            System.out.println(n+ "x" +(i+1)+ " =" +(n*(i+1)));
+        }
+    }
 }
