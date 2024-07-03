4f5b38c0b8e38a6f16c6e73648f4e203ecb0232c
bhagyashree <bhaggo30july@gmail.com>
2024-07-03 17:05:56 +0530

    convert the code for binary to decimal/integer

--- DecimalToBinary.java
+++ DecimalToBinary.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class DecimalToBinary {
+import java.util.Scanner;
+
+public class DecimalToBinary {
+    public static void main(String[] args) {
+        System.out.println("enter the binary digit ");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        int [] rem = new int[20];
+        int index =0;
+        while (num>0){
+            rem[index] = num %2;
+            index++;
+            num = num/2;
+
+        }
+        for (int i = index-1; i>=0; i--)
+        {
+            System.out.print(rem[i]);
+        }
+    }
 }
