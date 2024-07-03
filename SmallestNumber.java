d8adc66c84542f2097514da55a2572bc3aaafdb9
bhagyashree <bhaggo30july@gmail.com>
2024-07-03 12:37:18 +0530

    smallest number

--- SmallestNumber.java
+++ SmallestNumber.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class SmallestNumber {
+import java.util.Scanner;
+
+public class SmallestNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number to check smallest number");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if (a<b && a<c){
+            System.out.println(a+ " is the smallest number");
+        } else if (b<c) {
+            System.out.println(b+ " is the smallest number");
+        }
+        else {
+            System.out.println(c+ " is the smallest number");
+        }
+    }
 }
