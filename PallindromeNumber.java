8b3f11236f1d906d957e0ca199694155ad7beecb
bhagyashree <bhaggo30july@gmail.com>
2024-07-03 12:33:49 +0530

    check the number is pallindrome or not

--- PallindromeNumber.java
+++ PallindromeNumber.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class PallindromeNumber {
+import java.util.Scanner;
+
+public class PallindromeNumber {
+    public static boolean isPallindrome(int x){
+        int rev =0;
+        int temp = x;
+        while (x!=0) {
+            int r = x % 10;
+            rev = rev * 10 + r;
+            x = x / 10;
+        }
+        return rev == temp;
+    }
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("enter a number");
+        int n = scanner.nextInt();
+        boolean rs = isPallindrome(n);
+        if (rs == true){
+            System.out.println(n+ " is a pallindrome");
+        }
+        else{
+            System.out.println(n+ " is not a pallindrome");
+        }
+
+    }
 }
