ffe0ee4c83ffa1869b764685e92a3b61262f7612
bhagyashree <bhaggo30july@gmail.com>
2024-07-03 12:27:38 +0530

    calculator program

--- Calculator.java
+++ Calculator.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Calculator {
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter first number");
+        int a = sc.nextInt();
+        System.out.println("enter second number");
+        int b = sc.nextInt();
+        System.out.println("addition of two number "+(a+b));
+        System.out.println("subtraction of two number "+(a-b));
+        System.out.println("multiplication of two number is "+(a*b));
+        System.out.println("division of two number is "+(a%b));
+        System.out.println("modulus of two number is "+(a/b));
+
+
+
+    }
 }
