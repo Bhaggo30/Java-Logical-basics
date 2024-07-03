72eac18f874aa49a552d99fea3118883538d790b
bhagyashree <bhaggo30july@gmail.com>
2024-07-03 12:30:08 +0530

    check the number is even or odd

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class EvenOdd {
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int n = sc.nextInt();
+        if (n%2==0){
+            System.out.println(n+ " is a even number");
+        }
+        else {
+            System.out.println(n+ " is a odd number");
+        }
+    }
 }
