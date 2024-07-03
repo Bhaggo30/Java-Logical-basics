46a37e04eec21e1b0d17f16449c2f8c6293c42b2
bhagyashree <bhaggo30july@gmail.com>
2024-07-03 12:28:39 +0530

    check the particular integer is digit or a number

--- DigitOrNumber.java
+++ DigitOrNumber.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class DigitOrNumber {
+import java.util.Scanner;
+
+public class DigitOrNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the integer value");
+        int x = scanner.nextInt();
+        if (x >= -9  && x<=9){
+            System.out.println(x+ "is a digit");
+        }
+        else {
+            System.out.println(x+ " is a number");
+        }
+    }
 }
