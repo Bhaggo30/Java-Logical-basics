6118c52e1f97ffa52dafeec673bb020defb8e6ff
bhagyashree <bhaggo30july@gmail.com>
2024-07-03 15:43:34 +0530

    Addition of two binary

--- AddTwoBinary.java
+++ AddTwoBinary.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class AddTwoBinary {
-}
+import java.util.Scanner;
+public class AddTwoBinary {
+
+            public static void main(String[] args) {
+                int i = 0, remainder = 0;
+
+                int[] sum = new int[20];
+
+                Scanner in = new Scanner(System.in);
+                System.out.print("Input first binary number: ");
+                long binary1 = in.nextLong();
+
+                System.out.print("Input second binary number: ");
+                long binary2 = in.nextLong();
+                while (binary1 != 0 || binary2 != 0)
+                {
+                    sum[i++] = (int)((binary1 % 10 + binary2 % 10 + remainder) % 2);
+                    remainder = (int)((binary1 % 10 + binary2 % 10 + remainder) / 2);
+                    binary1 = binary1 / 10;
+                    binary2 = binary2 / 10;
+                }
+                if (remainder != 0) {
+                    sum[i++] = remainder;
+                }
+                --i;
+                System.out.print("Sum of two binary numbers: ");
+
+            }
+        }
+
+
+
+
