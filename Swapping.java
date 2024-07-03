682e3bc081be7876e28495761540c4254c4aedf3
bhagyashree <bhaggo30july@gmail.com>
2024-07-03 12:50:00 +0530

    swap the value

---
+++ Swapping.java
@@ -0,0 +1,12 @@
+public class Swapping {
+    public static void main(String[] args) {
+        int a, b, temp;
+        a=10;
+        b=20;
+        System.out.println("before swapping of the value of a and b is "+a+ " "+b);
+        temp =a;
+        a =b;
+        b=temp;
+        System.out.println("after swapping of two value is a and b is "+a+ " "+b);
+    }
+}
