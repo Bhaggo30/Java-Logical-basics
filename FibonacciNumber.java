b925d7223f65e1ddddad8eda5ad6c5b8f31901ad
bhagyashree <bhaggo30july@gmail.com>
2024-07-03 12:31:35 +0530

    print the fibonacci series

--- FibonacciNumber.java
+++ FibonacciNumber.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class FibonacciNumber {
+public class FibonacciNumber {
+    public static void printFibonacci(int n){
+        int f1 =0, f2 =1,count =0;
+        while (count<n){
+            System.out.println(f1+ " ");
+            int f3 = f1+f2;
+            f1 =f2;
+            f2 =f3;
+            count++;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("fibonacci series are");
+        printFibonacci(5);
+
+    }
 }
