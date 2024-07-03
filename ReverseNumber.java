e3ccd5ffb4ea65399485fc89fe27403e3d9c9a1e
bhagyashree <bhaggo30july@gmail.com>
2024-07-03 12:35:49 +0530

    reverse the number

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class ReverseNumber {
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static int reverse(int x){
+        int rev = 0;
+        while (x != 0) {
+            int r = x % 10;
+            rev = rev * 10 + r;
+            x = x / 10;
+        }
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = sc.nextInt();
+        int rev = reverse(n);
+        System.out.println("Reverse of the number: " + rev);
+    }
 }
