8de3e6b1850f142e08e69e51766f54eab70afbe1
bhagyashree <bhaggo30july@gmail.com>
2024-07-03 14:30:04 +0530

    range 1 to 100 print even odd

---
+++ EvenOdd100.java
@@ -0,0 +1,13 @@
+public class EvenOdd100 {
+    public static void main(String[] args) {
+        System.out.println("check number is even odd");
+        for (int i =0; i<=100; i++){
+            if (i%2==0){
+                System.out.println(i+ " is a even number");
+            }
+            else {
+                System.out.println(i+ " is a odd number");
+            }
+        }
+    }
+}
